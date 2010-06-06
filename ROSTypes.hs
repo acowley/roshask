8c808c19728a8ed6b73fc9325b484ca8377e2a3e
Anthony Cowley <acowley@tremolo>
2010-06-05 20:34:18 -0400

    First phase of working on the k-nucleotide benchmark game in Haskell. The first bits are mostly done, but the hard parts remain.

--- ROSTypes.hs
+++ ROSTypes.hs
@@ -1,13 +1,22 @@
 {-# LANGUAGE GADTs, ExistentialQuantification, ScopedTypeVariables #-}
 -- ROS Node implementation.
 module ROSTypes where
+import Control.Applicative
 import Data.Map (Map)
+import qualified Data.Map as M
 import Data.Set (Set)
 --import qualified Network.XmlRpc.Internals as XmlRpc
 import Data.Typeable
 import Control.Concurrent (ThreadId)
 import Control.Concurrent.Chan
-import System.IO (Handle)
+--import System.IO (Handle)
+import qualified Data.ByteString.Lazy as B
+import Data.Binary.Put
+import Data.Binary.Get
+import Data.Binary
+
+import System.IO.Unsafe (unsafeInterleaveIO)
+import Unsafe.Coerce
 
 type URI       = String
 type CallerID  = String
@@ -20,43 +29,58 @@ class TopicType t where
     name :: t -> String
     md5  :: t -> String
 
-class Subscriber t where
-    subscribeTo :: t -> URI -> IO ThreadId
+data Stream a = Stream a (Stream a)
 
-class TopicType t => Publisher t where
-    publishTo :: t -> (a -> IO ThreadId)
+--type Handler a b = Stream a -> Stream b
 
--- |A SubTopic has a function for connecting to a new publisher and the
--- set of all known publishers of this topic.
-data SubTopic a where
-    SubTopic :: Typeable a => (URI -> IO ThreadId) -> Set URI -> SubTopic a
+data Handler a b where
+    Handler :: Stream a -> Stream b -> Handler a b
 
-instance Typeable a => Typeable (SubTopic a) where
-    typeOf (SubTopic _ _) = mkTyConApp subCon [typeOf (undefined::a)]
-        where subCon = mkTyCon "ROS.SubTopic"
+data EHandler = forall a b. EHandler (Handler a b)
 
--- |A PubTopic has a list of actions for pushing new data to subscribers.
-data PubTopic a where
-    PubTopic :: Typeable a => [a -> IO ()] -> PubTopic a
+--data EStream = forall a. Binary a => EStream (Stream a)
 
-instance Typeable a => Typeable (PubTopic a) where
-    typeOf (PubTopic _) = mkTyConApp pubCon [typeOf (undefined::a)]
-        where pubCon = mkTyCon "ROS.PubTopic"
-
-instance TopicType a => TopicType (SubTopic a) where
-    name (SubTopic _ _) = name (undefined :: a)
-    md5 (SubTopic _ _) = md5 (undefined :: a)
-
-data ETopic = forall a. (TopicType a, Typeable a) => ETopic a
-
-instance Typeable ETopic where
-    typeOf (ETopic a) = typeOf a
-
+{-
 data Node = Node { master        :: URI
-                 , subscriptions :: Map TopicName ETopic
-                 , publications  :: Map TopicName ETopic
+                 , subscriptions :: Map TopicName (URI -> IO ())
+                 , publications  :: Map TopicName (Handle -> IO ())
                  , threads       :: [ThreadId]
                  , handles       :: [Handle] }
+-}
+
+newtype RosInt = RosInt { unRosInt :: Int } deriving Show
+
+instance Typeable RosInt where
+    typeOf _ = mkTyConApp (mkTyCon "RosInt") []
+
+instance Binary RosInt where
+    put = putWord32host . unsafeCoerce . unRosInt
+    get = unsafeCoerce <$> getWord32host
+
+handleInt :: Int -> IO ()
+handleInt x = putStrLn $ "Got int: " ++ show x
+
+src1 :: Stream RosInt
+src1 = listToStream $ cycle $ map RosInt [1,2,3]
+
+listToStream (x:xs) = Stream x (listToStream xs)
+
+data EStream = forall a. EChan (a -> IO ())
+
+data Node = Node { master :: URI
+                 , subscriptions :: Map TopicName EStream
+                 , publications :: Map TopicName EStream }
+
+testNode :: Node
+testNode = Node "home" (M.fromList [("nums", (EChan handleInt))]) M.empty
+
+--opaqueData :: B.ByteString
+opaqueData = runPut $ do mapM_ put (map RosInt [1,2,3])
+
+
+
+
+
 
 --, params :: Map String XmlRpc.Value }
 
