8683666ffe8d8c2b67246cc9801e15e83c5f48da
Anthony Cowley <acowley@seas.upenn.edu>
2010-06-08 19:54:39 -0400

    Started implementing a monadic Node interface.
    The issue is that I need a subscription to yield a pure stream,
    but said stream must actually live within IO itself.

--- Node.hs
+++ Node.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE PackageImports #-}
 module Node where
 import Control.Concurrent.STM.TVar (TVar)
+import "mtl" Control.Monad.State
+import Data.Binary
+import Data.Map (Map)
 import Data.Set (Set)
 import Data.Typeable (Typeable, TypeRep)
 import Control.Concurrent (ThreadId)
@@ -14,6 +18,30 @@ data Publication = Publication { subscribers :: Set URI
                                , pubType     :: TypeRep
                                , pubStats    :: TVar PubStats
                                , pubThread   :: ThreadId }
+
+data Topic = Topic { inputs :: [TopicName]
+                   , outputs :: [TopicName] }
+
+data NodeState = NodeState { nodeName      :: String
+                           , master        :: URI
+                           , subscriptions :: Map String Subscription
+                           , publications  :: Map String Publication }
+
+newtype Node a = Node { unNode :: StateT NodeState IO a }
+
+instance Monad Node where
+    (Node s) >>= f = Node $ s >>= unNode . f
+    return = Node . return
+
+subscribe :: (Typeable a, Binary a) => TopicName -> Node (Stream a)
+subscribe name = return undefined
+
+advertise :: (Typeable a, Binary a) => TopicName -> Stream a -> Node ()
+advertise name stream = return ()
+
+-- | A filtered stream.
+fooBar :: (Num a, Ord a) => Stream a -> Stream a
+fooBar (Stream x xs) = if x < 2 then fooBar xs else Stream x (fooBar xs)
 
 mkSub :: Typeable a => (Stream a -> b) -> Subscription
 mkSub handler = undefined
