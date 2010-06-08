8f5a56f35f70ebb4ad269cc349d942894a6e7302
Anthony Cowley <acowley@tremolo>
2010-06-07 21:14:41 -0400

    First real crack at most of the XML-RPC Slave API.

--- ROSTypes.hs
+++ ROSTypes.hs
@@ -1,86 +1,22 @@
-{-# LANGUAGE GADTs, ExistentialQuantification, ScopedTypeVariables #-}
--- ROS Node implementation.
+-- Utility types for working with ROS.
 module ROSTypes where
-import Control.Applicative
-import Data.Map (Map)
-import qualified Data.Map as M
-import Data.Set (Set)
---import qualified Network.XmlRpc.Internals as XmlRpc
-import Data.Typeable
-import Control.Concurrent (ThreadId)
-import Control.Concurrent.Chan
---import System.IO (Handle)
-import qualified Data.ByteString.Lazy as B
-import Data.Binary.Put
-import Data.Binary.Get
-import Data.Binary
-
-import System.IO.Unsafe (unsafeInterleaveIO)
-import Unsafe.Coerce
 
 type URI       = String
 type CallerID  = String
 type TopicName = String
-
--- |A reflective interface for topics that names the type of data
--- transmitted over the topic and includes the MD5 hash of the .msg
--- file the type is derived from.
-class TopicType t where
-    name :: t -> String
-    md5  :: t -> String
+type NodeName = String
+type TopicType = String
+type ConnectionID = Int
 
 data Stream a = Stream a (Stream a)
 
---type Handler a b = Stream a -> Stream b
+data SubStats = SubStats { bytesReceived  :: Int
+                         , subDestination :: URI
+                         , subConnected   :: Bool }
 
-data Handler a b where
-    Handler :: Stream a -> Stream b -> Handler a b
+data PubStats = PubStats { bytesSent      :: Int
+                         , numSent        :: Int
+                         , pubDestination :: URI
+                         , pubConnected   :: Bool }
 
-data EHandler = forall a b. EHandler (Handler a b)
-
---data EStream = forall a. Binary a => EStream (Stream a)
-
-{-
-data Node = Node { master        :: URI
-                 , subscriptions :: Map TopicName (URI -> IO ())
-                 , publications  :: Map TopicName (Handle -> IO ())
-                 , threads       :: [ThreadId]
-                 , handles       :: [Handle] }
--}
-
-newtype RosInt = RosInt { unRosInt :: Int } deriving Show
-
-instance Typeable RosInt where
-    typeOf _ = mkTyConApp (mkTyCon "RosInt") []
-
-instance Binary RosInt where
-    put = putWord32host . unsafeCoerce . unRosInt
-    get = unsafeCoerce <$> getWord32host
-
-handleInt :: Int -> IO ()
-handleInt x = putStrLn $ "Got int: " ++ show x
-
-src1 :: Stream RosInt
-src1 = listToStream $ cycle $ map RosInt [1,2,3]
-
-listToStream (x:xs) = Stream x (listToStream xs)
-
-data EStream = forall a. EChan (a -> IO ())
-
-data Node = Node { master :: URI
-                 , subscriptions :: Map TopicName EStream
-                 , publications :: Map TopicName EStream }
-
-testNode :: Node
-testNode = Node "home" (M.fromList [("nums", (EChan handleInt))]) M.empty
-
---opaqueData :: B.ByteString
-opaqueData = runPut $ do mapM_ put (map RosInt [1,2,3])
-
-
-
-
-
-
---, params :: Map String XmlRpc.Value }
 
--- SlaveAPI.hs
+++ SlaveAPI.hs
@@ -1,45 +1,149 @@
-module SlaveAPI (getSubscriptions, getPublications) where
+module SlaveAPI (RosSlave(..), runSlave) where
 import Control.Applicative
 import Control.Arrow (second)
-import qualified Data.Map as M
-import qualified Data.Set as S
-import Data.Typeable (cast)
+import Control.Concurrent (killThread, forkIO)
+import Control.Concurrent.QSem
+import Control.Monad
+import Control.Monad.Error (throwError)
+--import Network.Fancy
+import Network.HTTP.Server
+import Network.Socket (recv)
+import Network.XmlRpc.Internals
+import Network.XmlRpc.Server
+import System.IO (hGetContents, hPutStr, hClose)
+import System.Posix.Process (getProcessID)
 import ROSTypes
 
-data Slave = Slave { getSubscriptions :: [(TopicName, String)]
-                   , getPublications :: [(TopicName, String)]
-                   , publisherUpdate :: TopicName -> [URI] -> IO Slave }
+class RosSlave a where
+    getMaster :: a -> URI
+    getSubscriptions :: a -> [(TopicName, TopicType, [SubStats])]
+    getPublications :: a -> [(TopicName, TopicType, [PubStats])]
+    publisherUpdate :: a -> TopicName -> [URI] -> IO ()
+    getTopicPortTCP :: a -> TopicName -> Int
+    stopNode :: a -> IO ()
 
-mkSlave :: Node -> Slave
-mkSlave = Slave <$> getSubs <*> getPubs <*> pubUpdate'
+type MessageData = String
+type PublishStatsX = (TopicName, MessageData, [(Int, Int, Int, Bool)])
+type RpcResult a = IO (Int, String, a)
 
-topicType :: ETopic -> String
-topicType (ETopic t) = name t
+instance (XmlRpcType a, XmlRpcType b, XmlRpcType c, XmlRpcType d, 
+          XmlRpcType e) => 
+         XmlRpcType (a,b,c,d,e) where
+    toValue (v,w,x,y,z) = 
+        ValueArray [toValue v, toValue w, toValue x, toValue y, toValue z]
+    fromValue (ValueArray [v,w,x,y,z]) = 
+        liftM5 (,,,,) (fromValue v) (fromValue w) (fromValue x) 
+                      (fromValue y) (fromValue z) 
+    fromValue _ = throwError "Expected 5-element tuple!"
+    getType _ = TArray
 
-getSubs :: Node -> [(String, String)]
-getSubs = map (second topicType) . M.toList . subscriptions
 
-getPubs :: Node -> [(String, String)]
-getPubs = map (second topicType) . M.toList . publications
+instance (XmlRpcType a, XmlRpcType b, XmlRpcType c, XmlRpcType d) => 
+         XmlRpcType (a,b,c,d) where
+    toValue (w,x,y,z) = ValueArray [toValue w, toValue x, toValue y, toValue z]
+    fromValue (ValueArray [w,x,y,z]) = 
+        liftM4 (,,,) (fromValue w) (fromValue x) (fromValue y) (fromValue z)
+    fromValue _ = throwError "Expected 4-element tuple!"
+    getType _ = TArray
 
-pubUpdate :: Node -> TopicName -> [URI] -> Maybe (IO Node)
-pubUpdate n name publishers = 
-    do topic <- M.lookup name (subscriptions n) :: Maybe ETopic
-       (SubTopic doSub knownPubs) <- cast topic
-       let pubs' = filter (flip S.notMember knownPubs) publishers
-       case pubs' of
-         [] -> Nothing
-         _ -> return $ do ts <- mapM doSub pubs'
-                          let ts' = ts ++ threads n
-                              pubs'' = S.union knownPubs (S.fromList pubs')
-                              sub = SubTopic doSub pubs''
-                              subs' = M.insert name (ETopic sub) 
-                                               (subscriptions n)
-                          return $ n { threads = ts'
-                                     , subscriptions = subs' }
+instance (XmlRpcType a, XmlRpcType b, XmlRpcType c) => XmlRpcType (a,b,c) where
+    toValue (x,y,z) = ValueArray [toValue x, toValue y, toValue z]
+    fromValue (ValueArray [x,y,z]) = 
+        liftM3 (,,) (fromValue x) (fromValue y) (fromValue z)
+    fromValue _ = throwError "Expected 3-element tuple!"
+    getType _ = TArray
 
-pubUpdate' :: Node -> TopicName -> [URI] -> IO Slave
-pubUpdate' n t pubs = case pubUpdate n t pubs of
-                        Just updateNode -> do n' <- updateNode
-                                              return (mkSlave n')
-                        Nothing -> return $ mkSlave n
+instance (XmlRpcType a, XmlRpcType b) => XmlRpcType (a,b) where
+    toValue (x,y) = ValueArray [toValue x, toValue y]
+    fromValue (ValueArray [x,y]) = liftM2 (,) (fromValue x) (fromValue y)
+    fromValue _ = throwError "Expected 2-element tuple."
+    getType _ = TArray
+
+mkPublishStats :: (TopicName, a, [PubStats]) -> PublishStatsX
+mkPublishStats (n, _, pstats) = (n, "", map formatStats pstats)
+    where formatStats (PubStats bytesSent numSent _ conn) = 
+              (0, bytesSent, numSent, conn)
+
+mkSubStats :: (TopicName, a, [SubStats]) -> (String, [(Int, Int, Int, Bool)])
+mkSubStats (n, _, sstats) = (n, map formatStats sstats)
+    where formatStats (SubStats bytesReceived _ conn) = 
+              (0, bytesReceived, -1, conn)
+
+getBusStats :: (RosSlave a) => a -> CallerID -> RpcResult [[Value]]
+getBusStats n callerId = 
+    return (1, "", [publishStats, subscribeStats, serviceStats])
+    where serviceStats = []
+          publishStats = map (toValue . mkPublishStats) (getPublications n)
+          subscribeStats = map (toValue . mkSubStats) (getSubscriptions n)
+
+getBusInfo :: (RosSlave a) => a -> CallerID -> RpcResult [[Value]]
+getBusInfo n _ = 
+    return (1, "", map (map toValue) (pubs ++ subs))
+    where pubs = map formatPubs (getPublications n)
+          subs = map formatSubs (getSubscriptions n)
+          formatPubs (n, _, stats) = 
+              map (\c -> (0::Int, pubDestination c, "o", "TCPROS", n)) stats
+          formatSubs (n, _, stats) = 
+              map (\c -> (0::Int, subDestination c, "i", "TCPROS", n)) stats
+
+getMaster' :: RosSlave a => a -> CallerID -> IO (Int, String, URI)
+getMaster' n _ = return (1, "", getMaster n)
+
+shutdown' :: RosSlave a => a -> QSem -> CallerID -> IO (Int, String, Bool)
+shutdown' n q _ = stopNode n >> signalQSem q >> return (1, "", True)
+
+getPid' :: RpcResult Int
+getPid' = do pid <- getProcessID
+             return (1, "", fromEnum pid)
+
+getSubscriptions' :: RosSlave a => a -> CallerID -> RpcResult [(String, String)]
+getSubscriptions' n _ = return (1, "", subs)
+    where subs = map (\(n,t,_) -> (n,t)) (getSubscriptions n)
+
+getPublications' :: RosSlave a => a -> CallerID -> RpcResult [(String, String)]
+getPublications' n _ = return (1, "", pubs)
+    where pubs = map (\(n,t,_) -> (n,t)) (getPublications n)
+
+paramUpdate' :: RosSlave a => a -> CallerID -> String -> Value -> RpcResult Bool
+paramUpdate' n _ paramKey paramVal = do putStrLn "paramUpdate not implemented!"
+                                        return (1, "", True)
+
+pubUpdate :: RosSlave a => a -> CallerID -> TopicName -> [URI] -> RpcResult Bool
+pubUpdate n _ topic publishers = do publisherUpdate n topic publishers
+                                    return (1, "", True)
+
+requestTopic :: RosSlave a => a -> CallerID -> TopicName -> [[Value]] -> 
+                RpcResult Value
+requestTopic n _ topic protocols = return (1, "", toValue protocolInfo)
+    where protocolInfo = ("TCPROS", getTopicPortTCP n topic)
+
+runSlave :: RosSlave a => a -> IO ()
+runSlave n = do quitNow <- newQSem 0
+                let handleRequest = handleCall (dispatch quitNow)
+--                     http h _ = hGetContents h >>= handleRequest >>= hPutStr h
+--                     spec = serverSpec { address = IPv4 "" 9932 }
+--                 threads <- streamServer spec http
+                --sock <- listenOn (PortNumber (fromIntegral 9932)) 
+                --thread <- forkIO (acceptClients sock handleRequest)
+                waitQSem quitNow
+                --killThread thread
+                sClose sock
+    where dispatch q = methods [ ("getBusStats", fun (getBusStats n))
+                               , ("getBusInfo", fun (getBusInfo n))
+                               , ("getMasterUri", fun (getMaster' n))
+                               , ("shutdown", fun (shutdown' n q))
+                               , ("getPid", fun getPid')
+                               , ("getSubscriptions", fun (getSubscriptions' n))
+                               , ("getPublications", fun (getPublications' n))
+                               , ("paramUpdate", fun (paramUpdate' n))
+                               , ("publisherUpdate", fun (pubUpdate n))
+                               , ("requestTopic", fun (requestTopic n)) ]
+--           acceptClients s rpc = do (h, _, _) <- accept s
+--                                    request <- hGetContents h
+--                                    putStrLn $ "Got request "++request
+--                                    response <- rpc request
+--                                    putStrLn $ "Sending reponse "++response
+--                                    hPutStr h response
+--                                    hClose h
+--                                    acceptClients s rpc
+
