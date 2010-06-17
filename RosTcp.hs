f60bc9cbc7051b5f43c6510c1e14c2b100c2e7fb
Anthony Cowley <acowley@tremolo>
2010-06-17 19:25:57 -0400

    First complete version of RosTcp.
    Implemented subscription and publication over TCP sockets using
    Streams as the public caller interface.

--- RosTcp.hs
+++ RosTcp.hs
@@ -1,25 +1,80 @@
-module RosTcp where
-import Data.Binary (Binary)
-import Data.ByteString
-import Network
+module RosTcp (subStream, runServer) where
+import Control.Applicative ((<$>))
+import Control.Arrow ((***))
+import Control.Concurrent (forkIO, killThread)
+import Control.Concurrent.BoundedChan
+import Control.Concurrent.STM (atomically)
+import Control.Concurrent.STM.TVar
+import Control.Monad (forever, forM_)
+import Data.Binary (Binary, put)
+import Data.Binary.Put (runPut)
+import Data.ByteString.Lazy (ByteString)
+import Network.BSD (getHostByName, hostAddress)
+import Network.Socket hiding (send, sendTo, recv, recvFrom, Stream)
+import qualified Network.Socket as Sock
+import Network.Socket.ByteString.Lazy
+import System.IO (IOMode(ReadMode))
+
+import BinaryIter
 import ROSTypes
 
-connect :: Binary a => URI -> IO (Stream a)
-connect target = let (host,port) = parseURI target
-                 in do h <- connectTo host port 
+-- |Maximum number of items to buffer for each client.
+sendBufferSize :: Int
+sendBufferSize = 10
 
--- The server starts a thread that peels elements off the stream as
--- they become available and sends them to any connected clients. Is
--- that right? We need to accept connections from new clients, but we
--- want to record statistics and status for each connection, which
--- means that we need some feedback from here to the caller. We also
--- probably want to setup a channel for each client. When new items
--- come from the stream, we push them into each channel, which is then
--- serviced by a separate thread for each connected client.
-runServer :: Binary a => Stream a -> IO (ThreadId, Int)
-runServer s = do sock <- listenOn (PortNumber (fromIntegral freePort))
-                 let acceptClients = do accept sock 
-                 t <- forkIO (acceptClients sock)
-                 return (sock, t)
-    where acceptClients 
-                       
+-- |Push each item from this client's buffer over the connected
+-- socket.
+serviceClient :: BoundedChan ByteString -> Socket -> IO ()
+serviceClient c s = go
+    where go = readChan c >>= sendAll s >> go
+
+-- |Accept new client connections. A new send buffer is allocated for
+-- each new client and added to the client list along with an action
+-- for cleaning up the client connection.
+acceptClients :: Socket -> TVar [(IO (), BoundedChan ByteString)] -> IO ()
+acceptClients sock clients = forever acceptClient
+    where acceptClient = do (client,_) <- accept sock
+                            chan <- newBoundedChan sendBufferSize
+                            t <- forkIO $ serviceClient chan client
+                            let cleanup = shutdown client ShutdownBoth >>
+                                          killThread t
+                            atomically $ readTVar clients >>= 
+                                         writeTVar clients . ((cleanup,chan) :)
+
+-- |Publish each item obtained from a Stream to each connected client.
+pubStream :: Binary a => Stream a -> TVar [(b, BoundedChan ByteString)] -> IO ()
+pubStream s clients = forever $ go s
+    where go (Stream x xs) = let bytes = runPut (put x)
+                             in atomically (readTVar clients) >>=
+                                mapM_ (flip writeChan bytes . snd)
+
+-- |Connect to a publisher and return the stream of data it is
+-- publishing.
+subStream :: BinaryIter a => URI -> IO (Stream a)
+subStream target = do sock <- socket AF_INET Sock.Stream defaultProtocol
+                      ip <- hostAddress <$> getHostByName host
+                      connect sock $ SockAddrInet port ip
+                      h <- socketToHandle sock ReadMode
+                      streamIn h
+    where (host, port) = parseLocation target
+          parseLocation = (id *** fromIntegral.read) . break (== ':')
+
+-- |The server starts a thread that peels elements off the stream as
+-- they become available and sends them to all connected
+-- clients. Returns an action for cleanup up resources allocated by
+-- this publication server along with the port the server is listening
+-- on.
+runServer :: Binary a => Stream a -> IO (IO (), Int)
+runServer stream = withSocketsDo $ do
+                     sock <- socket AF_INET Sock.Stream defaultProtocol
+                     bindSocket sock (SockAddrInet aNY_PORT iNADDR_ANY)
+                     port <- fromIntegral <$> socketPort sock
+                     listen sock 5
+                     clients <- newTVarIO []
+                     acceptThread <- forkIO $ acceptClients sock clients
+                     pubThread <- forkIO $ pubStream stream clients
+                     let cleanup = atomically (readTVar clients) >>= 
+                                   sequence_ . map fst >> 
+                                   shutdown sock ShutdownBoth >>
+                                   killThread acceptThread
+                     return (cleanup, port)
