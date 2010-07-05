941e0b6c0ef6b3d8428657677a003ee9166acbb5
Anthony Cowley <acowley@tremolo>
2010-07-05 18:20:15 -0400

    Added support for relative paths to msg generation.

--- Msg/Main.hs
+++ Msg/Main.hs
@@ -1,10 +1,12 @@
 module Main (main) where
+import Control.Applicative
 import qualified Data.ByteString.Char8 as B
 import Data.Char (toUpper)
-import System.Directory (createDirectoryIfMissing)
+import System.Directory (createDirectoryIfMissing, getCurrentDirectory)
 import System.Environment (getArgs)
 import System.Exit (exitWith, ExitCode(..))
-import System.FilePath (replaceExtension, splitFileName, splitPath, (</>))
+import System.FilePath (replaceExtension, splitFileName, splitPath, 
+                        isRelative, (</>))
 import Msg.Types
 import Msg.Parse
 import Msg.Gen
@@ -25,8 +27,13 @@ generate fname = do r <- parseMsg fname
           pkgHier = B.pack $ "Ros." ++ init pkgName ++ "."
           d' = d </> "haskell" </> "Ros" </> pkgName
 
+canonicalizeName :: FilePath -> IO FilePath
+canonicalizeName fname = if isRelative fname
+                         then (</> fname) <$> getCurrentDirectory
+                         else return fname
+
 main = do args <- getArgs
           case args of
-            [name] -> generate name
+            [name] -> canonicalizeName name >>= generate
             _ -> do putStrLn "Expected 1 argument: path to .msg file"
                     exitWith (ExitFailure (-1))
