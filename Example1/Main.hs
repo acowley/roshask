4a58d658a079d8b53da4cb452b3dbc3c6b83e274
Anthony Cowley <acowley@tremolo>
2010-07-21 19:42:02 -0400

    Made the RosHask1 example progam's output change over time.

--- Example1/Main.hs
+++ Example1/Main.hs
@@ -4,15 +4,14 @@ import qualified Ros.Std_msgs.String as S
 import Ros.Node
 import Ros.RosTypes
 
-genMsg = do threadDelay 3000000
-            return s
-    where s = S.String "hi, guy"
+wait n = threadDelay (1000000 * n)
 
-publish = Stream genMsg publish
+publish = go 1
+    where go n = Stream (wait 3 >> return (S.String (show n ++ " HA HA HA")))
+                        (go (n+1))
 
 handle (Stream m ms) = putStrLn ("roskell got "++S._data m) >> handle ms
 
 main = runNode "/roskell" $ do advertiseIO "/MyMessage" publish
                                chat <- subscribe "/chat"
                                runHandler (handle chat)
-             --runNode "/roskell" (advertiseIO "/MyMessage" publish)
