50b0d259dc2c8ed8f4801703cabb66a7913c3aad
Anthony Cowley <acowley@seas.upenn.edu>
2010-07-23 16:10:23 -0400

    Added a third example program that is a pure stream transformer.
    This node demonstrates lifting a function on Streams into the Node
    monad to apply it directly to a subscription and then advertise it.

---
+++ Example3/Setup.hs
@@ -0,0 +1,21 @@
+import Control.Applicative
+import Data.List (intercalate)
+import Distribution.Simple
+import Distribution.Simple.Setup
+import Distribution.PackageDescription
+import System.Directory (getCurrentDirectory)
+import System.FilePath ((</>))
+
+import Ros.Build.DepFinder
+
+-- Add the message directories of the package's we are dependent on to
+-- GHC's path.
+addRosMsgPaths :: Args -> BuildFlags -> IO HookedBuildInfo
+addRosMsgPaths _ bf = 
+    do dir <- getCurrentDirectory
+       msgPaths <- map (</>"msg"</>"haskell") <$> findPackageDeps dir
+       writeFile ".ghci" $ ":set -i"++ intercalate ":" msgPaths
+       let binfo = emptyBuildInfo { hsSourceDirs = msgPaths }
+       return (Nothing, [("RosHask3", binfo)])
+
+main = defaultMainWithHooks $ simpleUserHooks { preBuild = addRosMsgPaths }
