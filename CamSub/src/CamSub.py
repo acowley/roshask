5faf49e9bb673b9e7ca0947ed30646c5cd68a2fb
Anthony Cowley <acowley@seas.upenn.edu>
2010-07-27 22:20:46 -0400

    Fixed bug of not starting the OpenCV window thread.

--- CamSub/src/CamSub.py
+++ CamSub/src/CamSub.py
@@ -6,19 +6,17 @@ from cv_bridge import CvBridge
 import cv
 
 bridge = CvBridge()
-win = False
 
 def show_image(msg):
-    global win, bridge
-    if not win:
-        win = True
-        cv.NamedWindow("Cam")
-    print "ShowImage"
-    cv.ShowImage("Cam", bridge.imgmsg_to_cv(msg,"mono8"))
+    global bridge
+    #cv.ShowImage("Cam", bridge.imgmsg_to_cv(msg,"mono8"))
+    cv.ShowImage("Cam", bridge.imgmsg_to_cv(msg,))
 
 if __name__ == '__main__':
     try:
         rospy.init_node('CamSub')
+        cv.NamedWindow("Cam")
+        cv.StartWindowThread()
         rospy.Subscriber("/cam", Image, show_image)
         rospy.spin()
         cv.DestroyWindow("Cam")
