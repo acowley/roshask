efba151c16ada7b2c37d0fc7de7d932a982d525d
Anthony Cowley <acowley@tremolo>
2010-07-28 11:49:17 -0400

    Debugging piping CamPub into Example4.
    When running diffImage in Example4, I get a stack overflow.
    When running transformImage, I guet a "Client did not include
    the topic type in its connection request." error when a client
    tries to connect. This is reminiscent of the troulbes with CamPub
    and threading, so is worth investigating.

--- Example4/src/Main.hs
+++ Example4/src/Main.hs
@@ -3,7 +3,6 @@ module Main (main) where
 import Control.Applicative
 import qualified Data.Stream as S
 import qualified Data.Vector.Storable as V
-import qualified Ros.Roslib.Header as H
 import Ros.Sensor_msgs.Image
 import Ros.Node
 
@@ -14,9 +13,14 @@ paired s = (,) <$> s <*> S.tail s
 
 transformImage img = img { _data = V.map (*2) (_data img) }
 
--- Function that computes the difference from one image to another.
-diffImage (i1,i2) = i2 { _data = V.zipWith (-) (_data i2) (_data i1) }
+absDiff :: Integral a => a -> a -> a
+absDiff x y = fromIntegral $ abs (x' - y')
+    where x' = fromIntegral x :: Int
+          y' = fromIntegral y :: Int
 
-main = runNode "/haskimg" $
-       advertise "/diff"  =<< S.map diffImage . paired <$> subscribe "/images"
-       --advertise "/diff"  =<< S.map transformImage <$> subscribe "/images"
+-- Function that computes the difference from one image to another.
+diffImage (i1,i2) = i2 { _data = V.zipWith absDiff (_data i2) (_data i1) }
+
+main = runNode "/differ" $
+       --advertise "/diff"  =<< fmap diffImage . paired <$> subscribe "/cam"
+       advertise "/diff"  =<< fmap transformImage <$> subscribe "/images"
