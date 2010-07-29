505f778e7c972953e89bc199350717e7297a9fd8
Anthony Cowley <acowley@seas.upenn.edu>
2010-07-29 19:29:48 -0400

    Major simplification to CamPub.
    Enabled threading in GHC's RTS and took out the more complex
    concurrency machinery from the CamPub program.

--- CamPub/src/Main.hs
+++ CamPub/src/Main.hs
@@ -1,7 +1,4 @@
 module Main (main) where
-import Control.Concurrent (forkIO)
-import Control.Concurrent.BoundedChan
-import Control.Monad (forever)
 import Ros.Node
 import Ros.Roslib.Header
 import Ros.Sensor_msgs.Image (Image(Image))
@@ -9,10 +6,8 @@ import AI.CV.OpenCV.HighGui (cvCreateCameraCapture, cvQueryFrame)
 import AI.CV.OpenCV.HIplImage (width, height, fromPtr)
 import AI.CV.OpenCV.PixelUtils (toMono)
 
--- NOTE: This program is very sensitive to threading. Namely, if there
--- are multiple threads (e.g. built with -threaded), then nothing
--- happens after the "Starting camera" message.
-pubImages chan = do putStrLn "Starting camera"
+-- Start the camera and return a stream of frame grabs.
+pubImageStream = do putStrLn "Starting camera"
                     capture <- cvCreateCameraCapture (-1)
                     let grab = do frame <- cvQueryFrame capture >>= fromPtr
                                   let w = fromIntegral $ width frame
@@ -20,10 +15,9 @@ pubImages chan = do putStrLn "Starting camera"
                                       pix = toMono frame
                                       head = Header 0 (0,0) ""
                                   return $ Image head h w "mono8" 0 w pix
-                    forever (grab >>= writeChan chan)
+                        grabStream = Cons grab grabStream
+                    return grabStream
 
-main = do chan <- newBoundedChan 1
-          t <- forkIO $ pubImages chan
-          let passAlong = Cons (readChan chan) passAlong
-          runNode "/CamPub" (advertiseIO "/cam" passAlong)
-          --release_capture capture
+main = do imageCap <- pubImageStream 
+          runNode "/CamPub" (advertiseIO "/cam" imageCap)
+
