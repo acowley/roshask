73c76b0c1f04095eb4e0763f3e27770003308b4c
Anthony Cowley <acowley@tremolo>
2010-07-30 10:32:13 -0400

    Simplified Example4 code and fixed dependency on StreamCombinators.

--- Example4/src/Main.hs
+++ Example4/src/Main.hs
@@ -5,29 +5,27 @@ import Data.Word (Word8)
 import qualified Data.Vector.Storable as V
 import Ros.Sensor_msgs.Image
 import Ros.Node
-import Ros.StreamCombinators (paired)
+import Ros.StreamCombinators (finiteDifference)
 
 -- Convert a 32-bit signed integer to an 8-bit unsigned integer.
-toAbsWord :: Int -> Word8
-toAbsWord = fromIntegral . abs
+toAbsWords :: V.Vector Int -> V.Vector Word8
+toAbsWords = V.map (fromIntegral . abs)
 
 -- Convert an 8-bit unsigned integer to a signed 32-bit integer.
-toInt :: Word8 -> Int
-toInt = fromIntegral
+toInts :: V.Vector Word8 -> V.Vector Int
+toInts = V.map fromIntegral
 
 -- Binarize integer values using a threshold.
 threshold x | x > 20 = 255
             | otherwise = 0
 
 -- Function that computes the difference from one image to another.
-diffImage (i1,i2) = i2 { _data = mask }
-    where mask = toBinary . toWords $ diff (toInts pixels1) (toInts pixels2)
-          pixels1 = _data i1
-          pixels2 = _data i2
-          toWords = V.map toAbsWord
-          toInts = V.map toInt
+diffImage i1 i2 = i2 { _data = mask }
+    where mask = toBinary . toAbsWords $ diff ipixels1 ipixels2
+          ipixels1 = toInts $ _data i1
+          ipixels2 = toInts $ _data i2
           diff = V.zipWith (-)
           toBinary = V.map threshold
 
 main = runNode "/differ" $
-       advertise "/diff"  =<< fmap diffImage . paired <$> subscribe "/cam"
+       advertise "/diff"  =<< finiteDifference diffImage <$> subscribe "/cam"
