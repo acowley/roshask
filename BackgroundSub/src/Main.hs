75a0b92da89d445c3e8fd28459ca625f3ff50e39
Anthony Cowley <acowley@tremolo>
2010-08-04 17:29:33 -0400

    Changed BackgroundSub to take advantage of HighCV fusion.
    Added a GHC flag to the build options to see which RULES fire.
    Removed ImageProc from BackgroundSub.

--- BackgroundSub/src/Main.hs
+++ BackgroundSub/src/Main.hs
@@ -7,7 +7,7 @@ import Ros.Node
 import Ros.Roslib.Header
 import Ros.Sensor_msgs.Image
 import Ros.StreamCombinators
-import ImageProc
+import qualified AI.CV.OpenCV.HighCV as H
 
 -- An IntImage has a width, a height, and some integer pixel data.
 data IntImage = IntImage Word32 Word32 !(V.Vector Int)
@@ -30,17 +30,18 @@ scale c (IntImage w h pix) = IntImage w h $ V.map (truncate.(*c).fromIntegral) p
 iscale c (IntImage w h pix) = IntImage w h $ V.map (*c) pix
 shift x (IntImage w h pix) = IntImage w h (V.map (`div` x) pix)
 
+despeckle w h pix = H.pixels . H.dilate 8 . H.erode 8 $ H.fromPixels w h pix
+
 maskMotion :: IntImage -> IntImage -> Image
 maskMotion (IntImage _ _ i1) (IntImage w h i2) = 
     intToImage $ IntImage w h (V.zipWith applyMask mask i1)
-    where mask = dilate w h 8 . erode w h 8 $ V.zipWith threshold diffs i2
+    where mask = despeckle w h $ V.zipWith threshold diffs i2
           applyMask m pix = if m > 0 then pix else 0
           diffs = V.map abs $ V.zipWith (-) i1 i2
           threshold diff avg = if diff > max 1 (avg `div` 32) then 255 else 0
 
 main = runNode "/backsub" $ do
        raw <- fmap toIntPixels <$> subscribe "/cam"
-       let --avg = weightedMean 0.9 add scale raw
-           avg = weightedMeanNormalized 7 1 add iscale (shift 8) raw
+       let avg = weightedMeanNormalized 7 1 add iscale (shift 8) raw
            streamMotion = fmap (uncurry maskMotion)
        advertise "/motion" $ streamMotion (lockstep (S.drop 1 raw) avg)
