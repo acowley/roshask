0f9d6bfb4f9904860b020676844faa28676571d3
Anthony Cowley <acowley@seas.upenn.edu>
2010-08-05 20:12:40 -0400

    Added support for publishing both monochrome and color images.
    Updated to new typed HOpenCV API.
    
    Compact design capable of publishing color or monochrome images.
    It is entirely possible that subscribing to both channels will not
    work well.

--- CamPub/src/Main.hs
+++ CamPub/src/Main.hs
@@ -1,35 +1,30 @@
 module Main (main) where
-import Control.Applicative ((<$>))
 import Ros.Node
 import Ros.Roslib.Header
 import Ros.Sensor_msgs.Image (Image(Image))
 import AI.CV.OpenCV.HighGui (cvCreateCameraCapture, cvQueryFrame)
-import AI.CV.OpenCV.HIplImage (width, height, fromPtr, pixels)
-import AI.CV.OpenCV.PixelUtils (toMono)
-
-pubMonoStream = do putStrLn "Starting camera"
-                   capture <- cvCreateCameraCapture (-1)
-                   let grab = do frame <- cvQueryFrame capture >>= fromPtr
-                                 let w = fromIntegral $ width frame
-                                     h = fromIntegral $ height frame
-                                     head = Header 0 (0,0) ""
-                                     pix = toMono frame
-                                 return $ Image head h w "mono8" 0 w pix
-                       grabStream = Cons grab grabStream
-                   return grabStream
+import AI.CV.OpenCV.HighCV (width, height, fromPtr, pixels, isColor)
+import AI.CV.OpenCV.PixelUtils (toMono, packPixels)
 
 -- Start the camera and return a stream of frame grabs.
-pubColorStream = do putStrLn "Starting camera"
-                    capture <- cvCreateCameraCapture (-1)
-                    let grab = do frame <- cvQueryFrame capture >>= fromPtr
-                                  let w = fromIntegral $ width frame
-                                      h = fromIntegral $ height frame
-                                      head = Header 0 (0,0) ""
-                                      pix = pixels frame
-                                  return $ Image head h w "bgr8" 0 (w*4) pix
-                        grabStream = Cons grab grabStream
-                    return grabStream
+grabStream = do putStrLn "Starting camera"
+                capture <- cvCreateCameraCapture (-1)
+                let grab = do frame <- cvQueryFrame capture >>= fromPtr
+                              let w = fromIntegral $ width frame
+                                  h = fromIntegral $ height frame
+                                  head = Header 0 (0,0) ""
+                              return (w,h,head, isColor frame)
+                    grabAll = Cons grab grabAll
+                return grabAll
 
-main = do imageCap <- pubMonoStream --pubColorStream
-          runNode "/CamPub" (advertiseIO "/cam" imageCap)
+pubMono (w,h,head,frame) = Image head h w "mono8" 0 w (toMono frame)
+--pubColor (w,h,head,frame) = Image head h w "bgr8" 0 (w*4) (pixels frame)
+pubColor (w,h,head,frame) = Image head h w "bgr8" 0 (w*3) (packPixels frame)
+
+main = do images <- grabStream
+          let monoStream = fmap (fmap pubMono) images
+              colorStream = fmap (fmap pubColor) images
+          runNode "/CamPub" $ do advertiseIO "/cam" monoStream
+                                 advertiseIO "/cam_color" colorStream
+                              
 
