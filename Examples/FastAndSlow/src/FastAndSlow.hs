95bdd98c2a071035b73276a91136281b1eb8c2d6
Anthony Cowley <acowley@tremolo>
2010-08-10 12:56:17 -0400

    Defined interpolation on Streams and wrote a quick test.
    The FastAndSlow module now has a definition for interpolation that
    is blind to any concrete notion of time stamps, or even literal ordering.
    
    Instead, this interpolation uses the disjunction of two streams (which
    produces a value whenever either component stream produces a value) to
    define an implicit ordering. Consecutive elements of the first stream
    are used to bracket elements of the second stream, all of which is
    handed off to a user-supplied interpolation function that returns a
    list of new values that are ultimately streamed out.

--- Examples/FastAndSlow/src/FastAndSlow.hs
+++ Examples/FastAndSlow/src/FastAndSlow.hs
@@ -1,7 +1,9 @@
 module FastAndSlow (main) where
 import Ros.Node
 import Ros.StreamCombinators
+import qualified Ros.Stream as S
 import Control.Concurrent
+import Data.Either (rights)
 import Data.Foldable (traverse_)
 import Data.IORef
 import System.IO.Unsafe
@@ -22,6 +24,35 @@ funnel = do fp <- streamIO fastProducer
             sp <- fmap (fmap fromEnum) $ streamIO slowProducer
             interleaved <- fp <|> sp
             consume interleaved
+
+discriminated :: Stream a -> Stream b -> IO (Stream (Either a b))
+discriminated xs ys = fmap Left xs <|> fmap Right ys
+
+-- Produce a Stream of the same type as the first but with the same
+-- production rate as the second, on average.
+interpolate :: (a -> a -> [b] -> [c]) -> Stream a -> Stream b -> IO (Stream c)
+interpolate f xs clock = 
+    do fused <- discriminated xs clock
+       let Cons (Left p1) xs' = S.dropWhile (not . isLeft) fused
+       return . concats $ go p1 xs'
+    where go p1 xs = let (window, Cons (Left p2) xs') = S.break isLeft xs
+                     in Cons (f p1 p2 (rights window)) (go p2 xs')
+          isLeft (Left _) = True
+          isLeft _ = False
+
+testInterp :: IO (Stream Float)
+testInterp = do slow <- streamIO slowNums :: IO (Stream Int)
+                fast <- streamIO fastUnits :: IO (Stream ())
+                interpolate f slow fast
+    where slowNums = let go x = Cons (threadDelay 5000000 >> return x) (go (x+1)) 
+                     in go 0
+          fastUnits = let go = Cons (threadDelay 500000 >> return ()) go
+                      in go
+          f :: Int -> Int -> [()] -> [Float]
+          f n1 n2 ticks = let n1' = fromIntegral n1
+                              n2' = fromIntegral n2
+                              step = (n2' - n1') / fromIntegral (length ticks)
+                          in [n1', n1' + step .. n2']
 
 main = do fp <- streamIO fastProducer
           sp <- streamIO slowProducer
