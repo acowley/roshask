880c60a4df1b375f5a97b35d3fc1129af8e45a58
Anthony Cowley <acowley@seas.upenn.edu>
2010-08-13 23:38:01 -0400

    ShowVideo is exhibiting a memory leak after a Stream is cut off.
    I have not been able to track down this leak yet. It may be
    piling up IO actions that handle each incoming item if,
    for example, an exception is being thrown on the socket and this
    is polluting the incoming stream.

--- Examples/ShowVideo/src/ShowVideo.hs
+++ Examples/ShowVideo/src/ShowVideo.hs
@@ -1,11 +1,12 @@
 module ShowVideo (main) where
 import Control.Applicative
+import Control.Concurrent
 import Control.Monad (forever, when)
 import Data.IORef (newIORef, readIORef, writeIORef)
 import qualified MySDL as SDL
 import SurfaceUtil
 import Ros.Node
-import Ros.Sensor_msgs.Image (Image, width, height, _data)
+import Ros.Sensor_msgs.Image (Image, width, height, _data, encoding)
 import Data.Foldable (traverse_)
 
 setVideo :: Int -> Int -> IO SDL.Surface
@@ -15,24 +16,33 @@ sdlInit :: IO (Image -> IO ())
 sdlInit = do _ <- SDL.init
              screen <- newIORef =<< setVideo 640 480
              res <- newIORef (640,480)
+             frameCount <- newIORef 0
              let showImage img = 
                      do let w = fromIntegral $ width img
                             h = fromIntegral $ height img
                             p = _data img
+                        n <- readIORef frameCount
+                        writeIORef frameCount $! n+1
+                        putStrLn $ "Frame "++show n
                         (w',h') <- readIORef res
                         when (w /= w' || h /= h')
                              (do writeIORef res (w,h)
                                  setVideo w h >>= writeIORef screen)
                         screen' <- readIORef screen
-                        image <- createGraySurfaceFrom [SDL.HWSurface] w h p
+                        image <- if encoding img == "mono8"
+                                 then createGraySurfaceFrom [SDL.HWSurface] w h p
+                                 else createRGBSurfaceFrom [SDL.HWSurface] w h p
                         SDL.blitSurface image Nothing screen' Nothing
                         SDL.flip screen'
                         _ <- SDL.pollEvent
                         return ()
              return showImage
 
+myTraverse :: (Image -> IO ()) -> Stream a -> IO ()
+myTraverse f (Cons x xs) = (f $! x) >> myTraverse f xs
+
 main = do showImage <- sdlInit
           runNode "ShowVideo" $
             do source <- getParam' "~video" "/video"
                images <- subscribe source
-               runHandler $ (traverse_ showImage images)
+               runHandler $ myTraverse showImage images --(traverse_ showImage images)
