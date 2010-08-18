fe2190e23cf2c18d447c071158c8da5b3b8964e8
Anthony Cowley <acowley@tremolo>
2010-08-18 11:58:30 -0400

    Fixed the rate limiter.

--- Ros/Rate.hs
+++ Ros/Rate.hs
@@ -1,27 +1,29 @@
+-- |Provides a rate limiting mechanism that can be used to control the
+-- rate at which 'IO' actions produce values.
 module Ros.Rate (rateLimiter) where
 import Control.Concurrent (threadDelay)
 import Control.Monad (when)
 import Data.IORef (newIORef, readIORef, writeIORef)
-import System.CPUTime (getCPUTime)
+import Data.Time.Clock (UTCTime, getCurrentTime, diffUTCTime)
 
-rateLimiter :: Float -> IO a -> IO (IO a)
-rateLimiter hz action = do prev <- newIORef 0
-                           return $ do t1 <- getCPUTime
-                                       x <- action
+timeDiff :: UTCTime -> UTCTime -> Double
+timeDiff = curry $ realToFrac . uncurry diffUTCTime
+
+-- |Produces an action that runs the supplied 'IO' action no faster
+-- than given rate in Hz.
+rateLimiter :: Double -> IO a -> IO (IO a)
+rateLimiter hz action = do prev <- getCurrentTime >>= newIORef
+                           est <- newIORef period
+                           return $ do t1 <- getCurrentTime
                                        t0 <- readIORef prev
-                                       let dt = fromIntegral $
-                                                (period - t1 + t0) `div` 1000000
-                                       when (dt > 0) $ threadDelay dt
+                                       estimate <- readIORef est
+                                       let error = timeDiff t1 t0 - period
+                                           estimate' = estimate*0.8 + 
+                                                       (estimate - error)*0.2
+                                           delay = truncate $ estimate' * 1e6
+                                       when (delay > 0) $ threadDelay delay
+                                       x <- action
                                        writeIORef prev t1
+                                       writeIORef est estimate'
                                        return x
-    where period = round $ 1e12 / hz
-
---rateLimiter :: Float -> IO (IO ())
--- rateLimiter hz = do prev <- newIORef 0
---                     return $ do t1 <- getCPUTime
---                                 t0 <- readIORef prev
---                                 let dt = fromIntegral $
---                                          (period - t1 + t0) `div` 1000000
---                                 when (dt > 0) $ threadDelay dt
---                                 writeIORef prev t1
---     where period = round $ 1e12 / hz
+    where period = 1 / hz
