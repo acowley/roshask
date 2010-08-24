73538ccf3b3c3d26225de5617a3811663c2e8d8b
Anthony Cowley <acowley@tremolo>
2010-08-24 14:16:38 -0400

    Added a rate limiting parameter to CamPub.

--- Examples/CamPub/src/Main.hs
+++ Examples/CamPub/src/Main.hs
@@ -1,30 +1,30 @@
 module Main (main) where
 import Ros.Node
+import Ros.Rate
 import Ros.Roslib.Header
 import Ros.Sensor_msgs.Image (Image(Image))
-import AI.CV.OpenCV.HighGui (cvCreateCameraCapture, cvQueryFrame)
-import AI.CV.OpenCV.HighCV (width, height, fromPtr, pixels, isColor)
+import AI.CV.OpenCV.HighCV (width, height, pixels, isColor, createCameraCapture)
 import AI.CV.OpenCV.PixelUtils (toMono, packPixels)
 
 -- Start the camera and return a stream of frame grabs.
-grabStream = do putStrLn "Starting camera"
-                capture <- cvCreateCameraCapture (-1)
-                let grab = do frame <- cvQueryFrame capture >>= fromPtr
-                              let w = fromIntegral $ width frame
-                                  h = fromIntegral $ height frame
-                                  head = Header 0 (0,0) ""
-                              return (w,h,head, isColor frame)
-                    grabAll = Cons grab grabAll
-                return grabAll
+grabStream rate = do putStrLn "Starting camera"
+                     capture <- createCameraCapture Nothing
+                     grab <- rateLimiter rate $ 
+                             do frame <- capture
+                                let w = fromIntegral $ width frame
+                                    h = fromIntegral $ height frame
+                                    head = Header 0 (0,0) ""
+                                return (w,h,head, isColor frame)
+                     let grabAll = Cons grab grabAll
+                     return grabAll
 
 pubMono (w,h,head,frame) = Image head h w "mono8" 0 w (toMono frame)
---pubColor (w,h,head,frame) = Image head h w "bgr8" 0 (w*4) (pixels frame)
 pubColor (w,h,head,frame) = Image head h w "bgr8" 0 (w*3) (packPixels frame)
 
-main = do images <- grabStream
-          let monoStream = fmap (fmap pubMono) images
-              colorStream = fmap (fmap pubColor) images
-          runNode "/CamPub" $ do advertiseIO "/cam" monoStream
-                                 advertiseIO "/cam_color" colorStream
+main = runNode "/CamPub" $ 
+       do rateCap <- getParam' "~rate" 30
+          images <- liftIO $ grabStream rateCap
+          advertiseIO "/cam" $ fmap (fmap pubMono) images
+          advertiseIO "/cam_color" $ fmap (fmap pubColor) images
                               
 
