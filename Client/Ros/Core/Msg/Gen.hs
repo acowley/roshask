cc5fc7ef6a09ffc4e3c1b5e4d25356955c26dd09
Anthony Cowley <acowley@seas.upenn.edu>
2010-10-13 16:48:50 -0400

    Fixed bug in Header support import generation.

--- Client/Ros/Core/Msg/Gen.hs
+++ Client/Ros/Core/Msg/Gen.hs
@@ -142,37 +142,39 @@ vectorDeps = S.fromList [ "qualified Data.Vector.Storable as V" ]
 -- unqualified names), the third is the 'MsgType' to generate imports
 -- for.
 typeDependency :: ByteString -> [ByteString] -> MsgType -> Set ByteString
-typeDependency _ _ RInt8             = intImport
-typeDependency _ _ RUInt8            = wordImport
-typeDependency _ _ RInt16            = intImport
-typeDependency _ _ RUInt16           = wordImport
-typeDependency _ _ RUInt32           = wordImport
-typeDependency _ _ RTime             = singleton "Ros.Core.RosTypes"
-typeDependency _ _ RDuration         = singleton "Ros.Core.RosTypes"
-typeDependency p m (RFixedArray _ t) = S.union vectorDeps $
+typeDependency _ _ RInt8                = intImport
+typeDependency _ _ RUInt8               = wordImport
+typeDependency _ _ RInt16               = intImport
+typeDependency _ _ RUInt16              = wordImport
+typeDependency _ _ RUInt32              = wordImport
+typeDependency _ _ RTime                = singleton "Ros.Core.RosTypes"
+typeDependency _ _ RDuration            = singleton "Ros.Core.RosTypes"
+typeDependency p m (RFixedArray _ t)    = S.union vectorDeps $
                                        typeDependency p m t
-typeDependency p m (RVarArray t)     = S.union vectorDeps $
+typeDependency p m (RVarArray t)        = S.union vectorDeps $
                                        typeDependency p m t
-typeDependency p m (RUserType ut)    = if elem ut m
-                                       then singleton $ 
-                                            B.concat ["qualified ", p, ut, 
-                                                      " as ", ut]
-                                       else path2Module ut
-typeDependency _ _ _                 = S.empty
+typeDependency _ _ (RUserType "Header") = 
+    S.fromList ["qualified Ros.Roslib.Header as Header", 
+                "Ros.Core.Msg.HeaderSupport"]
+typeDependency p m (RUserType ut)       = if elem ut m
+                                          then singleton $ 
+                                               B.concat ["qualified ", p, ut, 
+                                                         " as ", ut]
+                                          else path2Module ut
+typeDependency _ _ _                    = S.empty
 
 -- Non built-in types are either in the specified package or in the
 -- Ros.Std_msgs namespace. If a package path is given, then it is
 -- converted to a Haskell hierarchical module name and prefixed by
 -- "Ros.".
 path2Module :: ByteString -> Set ByteString
-path2Module "Header" = S.fromList ["qualified Ros.Roslib.Header as Header", 
-                                   "Ros.Core.Msg.HeaderSupport"]
-path2Module p = singleton $
-                if B.elem '/' p
-                then B.concat ["qualified Ros.",
+path2Module p 
+    | B.elem '/' p = singleton $
+                     B.concat ["qualified Ros.",
                                B.intercalate "." . map cap $ parts,
                                " as ", last parts]
-                else B.concat ["qualified Ros.Std_msgs.", p, " as ", p]
+    | otherwise    = singleton $
+                     B.concat ["qualified Ros.Std_msgs.", p, " as ", p]
     where cap s = B.cons (toUpper (B.head s)) (B.tail s)
           parts = B.split '/' p
 
