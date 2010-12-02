37f2bfd1933bae7df3301824d0d52f994a98ceb7
Anthony Cowley <acowley@tremolo>
2010-12-02 18:25:51 -0500

    Added /rosout logging to Turtle example.

--- Examples/Turtle/src/Turtle.hs
+++ Examples/Turtle/src/Turtle.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Turtle (main) where
 import Data.VectorSpace
 import Ros.Node
@@ -5,6 +6,7 @@ import Ros.Topic (cons, unfold)
 import Ros.TopicUtil (tee, filterBy, everyNew, interruptible, gate, toList, fromList)
 import Ros.Turtlesim.Pose
 import Ros.Turtlesim.Velocity
+import Ros.Logging
 import System.IO (hFlush, stdout)
 
 -- A type synonym for a 2D point.
@@ -12,16 +14,18 @@ type Point = (Float,Float)
 
 -- A Topic of user-supplied waypoint trajectories.
 getTraj :: Topic IO [Point]
-getTraj = unfold (putStr "Enter waypoints: " >> hFlush stdout >> read `fmap` getLine)
+getTraj = unfold (do putStr "Enter waypoints: " >> hFlush stdout
+                     $(logInfo "Got new traj")
+                     read `fmap` getLine)
 
 wrapAngle theta 
   | theta < 0    = theta + 2 * pi
   | theta > 2*pi = theta - 2 * pi
   | otherwise    = theta
 
-angleDiff x y = let x' = if x < 0 then x + 2*pi else x
-                    y' = if y < 0 then y + 2*pi else y
-                in wrapAngle (x' + pi - y') - pi
+angleDiff x y = wrapAngle (x' + pi - y') - pi
+  where x' = if x < 0 then x + 2*pi else x
+        y' = if y < 0 then y + 2*pi else y
 
 -- Navigate to a goal given a current pose estimate.
 navigate :: (Point, Pose) -> Velocity
@@ -32,7 +36,8 @@ navigate (goal, pos) = Velocity (min 2 (magnitude v)) angVel
         angVel       = signum thetaErr * (min 2 (abs thetaErr))
 
 main = runNode "HaskellBTurtle" $
-       do (p1, p2) <- liftIO . tee =<< subscribe "/turtle1/pose"
+       do enableLogging (Just Warn)
+          (p1, p2) <- liftIO . tee =<< subscribe "/turtle1/pose"
           (gs1, gs2) <- liftIO . tee . interruptible $ getTraj
           let arrived g p = magnitude (g ^-^ p) < 1.5
               p2v p = (x p, y p)
@@ -43,7 +48,7 @@ main = runNode "HaskellBTurtle" $
 
 -- Using lists. Notice that we've moved the explicit sharing burden
 -- from tee to fromList applications.
-main2 = runNode "HaskellBTurtle" $
+main' = runNode "HaskellBTurtle" $
        do ps <- liftIO . toList =<< subscribe "/turtle1/pose"
           gs <- liftIO . toList . interruptible $ getTraj
           let arrived g p = magnitude (g ^-^ p) < 1.5
