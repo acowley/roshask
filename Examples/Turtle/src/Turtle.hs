353f3d6451d62a5cb5e33d3851e297b3c0847ca9
Anthony Cowley <acowley@tremolo>
2010-12-02 02:23:12 -0500

    Added some more debugging helpers to Turtle.

--- Examples/Turtle/src/Turtle.hs
+++ Examples/Turtle/src/Turtle.hs
@@ -5,6 +5,8 @@ import Ros.Topic (cons, uncons, unfold)
 import Ros.TopicUtil (tee, filterBy, everyNew, interruptible)
 import Ros.Turtlesim.Pose
 import Ros.Turtlesim.Velocity
+import System.IO (hFlush, stdout)
+import Control.Concurrent (threadDelay)
 
 -- A type synonym for a 2D point.
 type Point = (Float,Float)
@@ -14,23 +16,27 @@ showPose = putStrLn . show
 
 -- A Topic of user-supplied waypoint trajectories.
 getTraj :: Topic IO [Point]
-getTraj = unfold (putStr "Enter waypoints: " >> read `fmap` getLine)
+getTraj = unfold (putStr "Enter waypoints: " >> hFlush stdout >> read `fmap` getLine)
 
 -- Navigate to a goal given a current pose estimate.
 navigate :: (Point, Pose) -> Velocity
-navigate (goal, pos) = Velocity 0.1 angVel
+navigate (goal, pos) = Velocity 2 angVel
   where (vx, vy)     = goal ^-^ (x pos, y pos)
         thetaDesired = atan2 vy vx * (180 / pi)
         thetaErr     = thetaDesired - theta pos
-        angVel       = signum thetaErr * (min 5 thetaErr)
+        angVel       = signum thetaErr * (min 2 (abs thetaErr))
 
--- main = runNode "HaskellBTurtle" $ 
---        runHandler showPose =<< subscribe "/turtle1/pose"
+posePrinter = runNode "HaskellBTurtle" $ 
+              runHandler showPose =<< subscribe "/turtle1/pose"
+
+-- Just move forward
+trans = runNode "Go" $ advertise "/turtle1/command_velocity" go
+  where go = Topic (threadDelay 1000000 >> return (Velocity 2 0, go))
 
 main = runNode "HaskellBTurtle" $
        do (p1, p2) <- liftIO . tee =<< subscribe "/turtle1/pose"
           (g1, gs) <- liftIO $ uncons (interruptible getTraj)
-          let arrived g p = magnitudeSq (g ^-^ p) < 0.2
+          let arrived g p = magnitudeSq (g ^-^ p) < 1.5
               p2v p = (x p, y p)
-              arrivals = cons g1 (filterBy (fmap arrived gs) (fmap p2v p1))
-          advertise "vel" $ fmap navigate (everyNew arrivals p2)
+              arrivals = cons g1 (filterBy (fmap arrived (cons g1 gs)) (fmap p2v p1))
+          advertise "/turtle1/command_velocity" $ fmap navigate (everyNew arrivals p2)
