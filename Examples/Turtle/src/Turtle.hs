8eefb3130382f7248c5830b5ca476c3951b0d487
Anthony Cowley <acowley@tremolo>
2010-12-02 15:07:53 -0500

    Cleanup of Turtle and show use of toList vs tee.

--- Examples/Turtle/src/Turtle.hs
+++ Examples/Turtle/src/Turtle.hs
@@ -1,26 +1,23 @@
 module Turtle (main) where
 import Data.VectorSpace
 import Ros.Node
-import Ros.Topic (cons, uncons, unfold)
-import Ros.TopicUtil (tee, filterBy, everyNew, interruptible, gate)
+import Ros.Topic (cons, unfold)
+import Ros.TopicUtil (tee, filterBy, everyNew, interruptible, gate, toList, fromList)
 import Ros.Turtlesim.Pose
 import Ros.Turtlesim.Velocity
 import System.IO (hFlush, stdout)
-import Control.Concurrent (threadDelay)
 
 -- A type synonym for a 2D point.
 type Point = (Float,Float)
-
-showPose :: Pose -> IO ()
-showPose = putStrLn . show
 
 -- A Topic of user-supplied waypoint trajectories.
 getTraj :: Topic IO [Point]
 getTraj = unfold (putStr "Enter waypoints: " >> hFlush stdout >> read `fmap` getLine)
 
-wrapAngle theta | theta < 0 = theta + 2 * pi
-wrapAngle theta | theta > 2*pi = theta - 2 * pi
-wrapAngle theta = theta
+wrapAngle theta 
+  | theta < 0    = theta + 2 * pi
+  | theta > 2*pi = theta - 2 * pi
+  | otherwise    = theta
 
 angleDiff x y = let x' = if x < 0 then x + 2*pi else x
                     y' = if y < 0 then y + 2*pi else y
@@ -43,3 +40,16 @@ main = runNode "HaskellBTurtle" $
               goals = gate gs2 (cons () (fmap (const ()) arrivals))
           advertise "/turtle1/command_velocity" $ 
                     fmap navigate (everyNew goals p2)
+
+-- Using lists. Notice that we've moved the explicit sharing burden
+-- from tee to fromList applications.
+main2 = runNode "HaskellBTurtle" $
+       do ps <- liftIO . toList =<< subscribe "/turtle1/pose"
+          gs <- liftIO . toList . interruptible $ getTraj
+          let arrived g p = magnitude (g ^-^ p) < 1.5
+              p2v p = (x p, y p)
+              arrivals = filterBy (fromList (map arrived gs)) 
+                                  (fromList (map p2v ps))
+              goals = gate (fromList gs) (cons () (fmap (const ()) arrivals))
+          advertise "/turtle1/command_velocity" $ 
+                    fmap navigate (everyNew goals (fromList ps))
