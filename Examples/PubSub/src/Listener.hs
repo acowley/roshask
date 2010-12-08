a0f125511cf581e850d09e3686d7c6d1b8a3726a
Anthony Cowley <acowley@tremolo>
2010-12-08 16:01:59 -0500

    Renamed Lister.callback to showMsg to avoid confusion.

--- Examples/PubSub/src/Listener.hs
+++ Examples/PubSub/src/Listener.hs
@@ -2,7 +2,7 @@ module Listener (main) where
 import Ros.Node
 import qualified Ros.Std_msgs.String as S
 
-callback :: S.String -> IO ()
-callback = putStrLn . ("I heard " ++) . S._data
+showMsg :: S.String -> IO ()
+showMsg = putStrLn . ("I heard " ++) . S._data
 
-main = runNode "listener" $ runHandler callback =<< subscribe "chatter"
+main = runNode "listener" $ runHandler showMsg =<< subscribe "chatter"
