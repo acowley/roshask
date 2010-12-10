41625b38f4ea30e52e33ecb081ea64248020b0ac
Anthony Cowley <acowley@tremolo>
2010-12-10 12:44:23 -0500

    Added strict StateT version of TopicMT.runTopicState'

--- Ros/TopicMT.hs
+++ Ros/TopicMT.hs
@@ -4,15 +4,22 @@
 -- stateful action to produce a 'Topic''s values.
 module Ros.TopicMT where
 import Control.Arrow
-import "monads-fd" Control.Monad.State.Lazy
+import qualified "monads-fd" Control.Monad.State.Lazy as L
+import qualified "monads-fd" Control.Monad.State.Strict as S
 import "monads-fd" Control.Monad.Reader
 import Ros.Topic
 
--- |Run a 'Topic' built around a 'StateT s' monad using a given
--- initial state.
-runTopicState :: Monad m => Topic (StateT s m) a -> s -> Topic m a
-runTopicState t s = Topic $ do ((x,t'), s') <- runStateT (runTopic t) s
+-- |Run a 'Topic' built around a lazy @'L.StateT' s@ monad using a
+-- given initial state.
+runTopicState :: Monad m => Topic (L.StateT s m) a -> s -> Topic m a
+runTopicState t s = Topic $ do ((x,t'), s') <- L.runStateT (runTopic t) s
                                return (x, runTopicState t' s')
+
+-- |Run a 'Topic' build around a strict @'S.StateT' s@ monad using a
+-- given initial state.
+runTopicState' :: Monad m => Topic (S.StateT s m) a -> s -> Topic m a
+runTopicState' t s = Topic $ do ((x,t'), s') <- S.runStateT (runTopic t) s
+                                return (x, runTopicState' t' s')
 
 -- |Run a 'Topic' built around a 'ReaderT r' monad using a value for
 -- reading.
@@ -20,8 +27,8 @@ runTopicReader :: (Functor m, Monad m) => Topic (ReaderT r m) a -> r -> Topic m 
 runTopicReader t r = go t
   where go (Topic ma) = Topic $ second go `fmap` runReaderT ma r
 
--- |Map a function over a 'Topic', in the process lifting the 'Topic'
--- into a new monad.
+-- |Map a monadic function over a 'Topic', in the process lifting the
+-- 'Topic' into a new monad.
 liftMap :: (MonadTrans t, Monad m, Monad (t m)) => 
            (a -> t m b) -> Topic m a -> Topic (t m) b
 liftMap f = go
