687a2cb7f34d5b31630c77664d2785dc23e1bf27
Anthony Cowley <acowley@tremolo>
2010-12-12 18:47:15 -0500

    Fixed issue with Storable instance for fixed vector msg types.

--- Ros/Core/Msg/StorableInstance.hs
+++ Ros/Core/Msg/StorableInstance.hs
@@ -8,6 +8,15 @@ import Data.Maybe (fromJust)
 import Ros.Core.Msg.Types
 import Ros.Core.Msg.Analysis
 
+rosPoke :: MsgType -> ByteString
+rosPoke (RFixedArray _ t) = B.concat [ "V.mapM_ (", rosPoke t, ")" ]
+rosPoke _ = "SM.poke"
+
+rosPeek :: MsgType -> ByteString
+rosPeek (RFixedArray n t) = B.concat [ "V.replicateM ", B.pack (show n), 
+                                       " (", rosPeek t, ")" ]
+rosPeek _ = "SM.peek"
+
 genStorableInstance :: Msg -> MsgInfo (ByteString, ByteString)
 genStorableInstance msg 
   | null (fields msg) = return ("import Foreign.Storable (Storable(..))\n",
@@ -16,9 +25,14 @@ genStorableInstance msg
     where aux False = return ("", "")
           aux True = do sz <- totalSize msg
                         return (smImp, stInst sz)
-          peekFields = map (const "SM.peek") (fields msg)
-          pokeFields = map ((\n -> B.concat ["SM.poke (", n, " obj')"]) . 
-                            fieldName)
+          --peekFields = map (const "SM.peek") (fields msg)
+          peekFields = map (rosPeek . fieldType) (fields msg)
+          -- pokeFields = map ((\n -> B.concat ["SM.poke (", n, " obj')"]) . 
+          --                   fieldName)
+          --                  (fields msg)
+          pokeFields = map (\f -> B.concat [ rosPoke (fieldType f)
+                                           , " (", fieldName f
+                                           , " obj')"])
                            (fields msg)
           name = pack (shortName msg)
           stInst sz = B.concat ["instance Storable ", name, " where\n",
