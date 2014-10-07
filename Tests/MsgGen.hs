904c12270b097233cd4e06aecf7c234c3a9597bb
Robbie Gleichman <rgleichman@gmail.com>
2014-10-06 17:30:24 -0700

    Fixed testGeneratedHaskell to take lists so that the Haskell types can be generated simultaneously. The gen tests now pass, still need to update md5 specific tests.

--- Tests/MsgGen.hs
+++ Tests/MsgGen.hs
@@ -2,6 +2,7 @@
 module MsgGen where
 import Analysis (addMsg)
 import Control.Applicative
+import Control.Monad (zipWithM)
 import Control.Monad.IO.Class
 import Control.Monad.State (evalStateT)
 import qualified Data.ByteString.Char8 as B
@@ -15,6 +16,7 @@ import System.FilePath ((</>), dropExtension, takeFileName)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Types (Msg)
+
 
 -- | The location of actionlib message definitions.
 actionDir :: FilePath
@@ -48,7 +50,7 @@ actionResources msgNames = do
   return (msgs,gold)
   where msgFiles = map (((actionDir</>"msg")</>) . (++".msg")) msgNames
 
-{-
+
 -- | Test the actionlib_msgs package's message definitions.
 testActionMsgs :: MsgInfo TestTree
 testActionMsgs =
@@ -69,25 +71,52 @@ testActionMsgs =
         msgDefs = [ ("GoalID", "302881f31927c1df708a2dbab0e80ee8")
                   , ("GoalStatus", "d388f9b87b3c471f784434d671988d4a")
                   , ("GoalStatusArray", "8b2b82f13216d0a8ea88bd3af735e619") ]
--}
-testActionMsgs :: MsgInfo TestTree
-testActionMsgs =
+
+-- TODO: Generated Haskell2 on GoalStatus fails to find GoalID, need to generate these files simultaneously.
+-- | Test the actionlib_msgs package's message definitions.
+testActionMsgs3 :: MsgInfo TestTree
+testActionMsgs3 =
   do (msgs,gold) <- liftIO $ actionResources (map fst msgDefs)
      cachePkg actionDir
-     --mapM_ addMsg msgs
-     md5TestList <- mapM (uncurry testMD5) $ zip msgFiles md5s
-     let md5Tests = testGroup "MD5s" md5TestList
+     mapM_ addMsg msgs
+     md5s <- mapM msgMD5 msgs
+     let md5Tests = testGroup "MD5" $
+                    zipWith (\(n,m) m' -> testCase n $ m @=? m') msgDefs md5s
      msgGen <- mapM (generateMsgType "Ros.Actionlib_msgs."
                                      (map (B.pack . fst) msgDefs))
                     msgs
      let genTests = testGroup "Generated Haskell" $
                     zipWith3 (\n g g' -> testCase n $ g @=? g') 
                              (map fst msgDefs) gold msgGen
-     return $ testGroup "actionlib_msgs" [md5Tests, genTests]
+     genTestList <- (testGeneratedHaskell "Ros.Actionlib_msgs.") msgFiles haskellFiles (map B.pack msgNames)
+     let genTests2 = testGroup "Generated Haskell2" [genTestList]
+     return $ testGroup "actionlib_msgs" [md5Tests, genTests, genTests2]
   where -- Message names and the corresponding MD5s computed by rosmsg
     msgNames = map fst msgDefs
-    md5s = map snd msgDefs
+    --md5s = map snd msgDefs
     msgFiles = map (((actionDir</>"msg")</>) . (++".msg")) msgNames
+    haskellFiles =  map ((goldenDir </>) . (++".hs")) msgNames
+    msgDefs = [ ("GoalID", "302881f31927c1df708a2dbab0e80ee8")
+              , ("GoalStatus", "d388f9b87b3c471f784434d671988d4a")
+              , ("GoalStatusArray", "8b2b82f13216d0a8ea88bd3af735e619") ]
+    
+
+testActionMsgs2 :: MsgInfo TestTree
+testActionMsgs2 =
+  do (msgs,gold) <- liftIO $ actionResources (map fst msgDefs)
+     cachePkg actionDir
+     mapM_ addMsg msgs
+     --md5TestList <- zipWithM testMD5 msgFiles md5s
+     --let md5Tests = testGroup "MD5s" md5TestList
+     --genTestList <- sequence $ zipWith3 (testGeneratedHaskell "Ros.Actionlib_msgs.") msgFiles haskellFiles (map B.pack msgNames)
+     genTests <- testGeneratedHaskell "Ros.Actionlib_msgs." msgFiles haskellFiles (map B.pack msgNames)
+     --let genTests = testGroup "Generated Haskell" genTestList
+     return $ testGroup "actionlib_msgs" [genTests]--[md5Tests, genTests]
+  where -- Message names and the corresponding MD5s computed by rosmsg
+    msgNames = map fst msgDefs
+    --md5s = map snd msgDefs
+    msgFiles = map (((actionDir</>"msg")</>) . (++".msg")) msgNames
+    haskellFiles =  map ((goldenDir </>) . (++".hs")) msgNames
     msgDefs = [ ("GoalID", "302881f31927c1df708a2dbab0e80ee8")
               , ("GoalStatus", "d388f9b87b3c471f784434d671988d4a")
               , ("GoalStatusArray", "8b2b82f13216d0a8ea88bd3af735e619") ]
@@ -115,9 +144,11 @@ testStringMsg = do
       messageFile = "Tests/test_msgs/msg/StringConst.msg"
       haskellFile = "Tests/test_msgs/golden/StringConst.hs"
   cachePkg testDir
-  gen <- testGeneratedHaskell messageFile haskellFile "StringConsts"
-  md5 <- testMD5 messageFile "a8e1a25e612660c2e8d3d161e9e91950"
-  return $ testGroup "String constants" [gen, md5]
+  gen <- testGeneratedHaskell "Ros.Test_msgs." [messageFile] [haskellFile] ["StringConsts"]
+  -- todo, test md5 again
+  --md5 <- testMD5 messageFile "a8e1a25e612660c2e8d3d161e9e91950"
+  --return $ testGroup "String constants" [gen, md5]
+  return $ testGroup "String constants"  [gen]
 
 
 -- | Test that the generated Haskell code matches a golden Haskell file
@@ -125,16 +156,33 @@ testStringMsg = do
 -- | The second argument is the path to the message file
 -- | The third argument is the path form to the golden Haskell file
 -- | Precondition: cachePkg has been called
-testGeneratedHaskell :: FilePath -> FilePath -> B.ByteString -> MsgInfo TestTree
-testGeneratedHaskell msgPath haskellPath moduleName =
+testGeneratedHaskell_old :: B.ByteString -> FilePath -> FilePath -> B.ByteString -> MsgInfo TestTree
+testGeneratedHaskell_old packagePath msgPath haskellPath moduleName =
   do --cachePkg testDirectory
      msg <- liftIO $ 
             either error id <$> parseMsg msgPath
      gold <- liftIO $ B.readFile haskellPath
-     -- <- addMsg msg
-     gen <- generateMsgType "Ros.Test_msgs." [moduleName] msg
+     _ <- addMsg msg
+     gen <- generateMsgType packagePath [moduleName] msg
      return $ testCase "Generated Haskell" $ gold @=? gen
 
+
+-- todo Make this work with lists of messages so that multiple messages can be added before "generateMsgType" is called
+-- | Test that the generated Haskell code matches a golden Haskell file
+-- | The first argument is a path to the test directory
+-- | The second argument is the path to the message file
+-- | The third argument is the path form to the golden Haskell file
+-- | Precondition: cachePkg has been called
+testGeneratedHaskell :: B.ByteString -> [FilePath] -> [FilePath] -> [B.ByteString] -> MsgInfo TestTree
+testGeneratedHaskell packagePath msgPaths haskellPaths moduleNames =
+  do --cachePkg testDirectory
+     msgs <- liftIO $ fmap (fmap (either error id)) $ mapM parseMsg msgPaths
+     golds <- liftIO $ mapM B.readFile haskellPaths
+     mapM_ addMsg msgs
+     gens <- mapM (generateMsgType packagePath moduleNames) msgs
+     return $ testGroup "Generated Haskell" $ zipWith3 (\name gold gen -> testCase (B.unpack name) $ gold @=? gen) moduleNames golds gens
+
+-- todo, integrate this with testGeneratedHaskell?
 -- | Precondition: cachePkg has been called
 testMD5 :: FilePath -> String -> MsgInfo TestTree
 testMD5 msgPath  md5 =
@@ -147,5 +195,6 @@ testMD5 msgPath  md5 =
 tests :: IO TestTree
 tests = fmap (testGroup "Message generation") $
         flip evalStateT emptyMsgContext $ do
-          prepMsgGen
-          sequence [testActionMsgs, testStringMsg]
+          --prepMsgGen
+          sequence [testActionMsgs3]
+                   --, testStringMsg]
