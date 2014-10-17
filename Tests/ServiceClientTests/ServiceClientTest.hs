069341c1a4219ed8ac5ba141169d9a75e2d4bfb3
Robbie Gleichman <rgleichman@gmail.com>
2014-10-16 20:34:04 -0700

    Rename TCPTest.hs

---
+++ Tests/ServiceClientTests/ServiceClientTest.hs
@@ -0,0 +1,16 @@
+--import qualified Ros.Msg.Int64 as I
+import Ros.Test_srvs.AddTwoIntsRequest
+import Ros.Test_srvs.AddTwoIntsResponse
+import Ros.Node.RosTcp (callService)
+import Ros.Internal.RosBinary
+
+
+main :: IO ()
+main = clientTest
+
+clientTest :: IO ()
+clientTest = do res <- (callService rosMast "/add_two_ints" (AddTwoIntsRequest{a= 42, b= 9})) :: IO (Maybe AddTwoIntsResponse)
+                case res of Nothing -> putStrLn "No result"
+                            Just item -> print res
+  where
+    rosMast = "http://localhost:11311/"
