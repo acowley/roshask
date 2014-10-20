c838b4a333af851d3d5b8afe12d55728da0b5742
Robbie Gleichman <rgleichman@gmail.com>
2014-10-19 21:44:46 -0700

    Add ServiceTypes

---
+++ src/Ros/Service/ServiceTypes.hs
@@ -0,0 +1,17 @@
+module Ros.Service.ServiceTypes (ServiceResponseError(..)) where
+
+import Control.Monad.Error
+
+-- | This type represensts the possible error cases that can occur when a service is called by a client.
+-- A NotOkError occurs when the server replies to a service request with an error message instead of the normal
+-- service message. The NotOkError string a string sent from the server. See http://wiki.ros.org/ROS/TCPROS
+-- A ResponseReadError occurs when the roshask service client has problems recieving either the expected service
+-- message or NotOkError message.
+-- MasterError is for problems encountered while communicating with the master
+-- ConHeadError is for an error with the connection header
+data ServiceResponseError = NotOkError String | ResponseReadError String | MasterError String | ConHeadError String
+                          deriving (Show, Eq)
+
+instance Error ServiceResponseError where
+  noMsg = ResponseReadError ""
+  strMsg x = ResponseReadError x
